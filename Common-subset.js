23687bfbef6e017317040f5306f16ca581e3421d
Ranjitha R <ranjitharaja5@gmail.com>
2019-10-11 13:35:01 +0530

    Update Common-subset.js

--- Common-subset.js
+++ Common-subset.js
@@ -1,6 +1,6 @@
 function printSubsetSum(arr, i, currentSum, sum, eachSet, result){
  
-  if(currentSum == sum){
+  if(currentSum == sum && i==arr.length){
     var val = [...eachSet]
     result.push(val);
     // console.log(result, i)
@@ -16,6 +16,6 @@ function printSubsetSum(arr, i, currentSum, sum, eachSet, result){
   return result
 }
 
-var finalResult = printSubsetSum([3,2,6,1], 0, 0, 9, [], [])
+var finalResult = printSubsetSum([3,34,4,12,5,2], 0, 0, 9, [], [])
 
 console.log(finalResult)
