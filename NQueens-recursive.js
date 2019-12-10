04e244bc85a1378ace83ccba05fc9f1bf6ba7f6c
Ranjitha Raja <ranjitharaja5@gmail.com>
2019-12-10 22:40:31 +0530

    Update NQueens-recursive.js

--- NQueens-recursive.js
+++ NQueens-recursive.js
@@ -1,20 +1,20 @@
 //The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
 
-Example:
+// Example:
 
-Input: 4
-Output: [
- [".Q..",  // Solution 1
-  "...Q",
-  "Q...",
-  "..Q."],
+// Input: 4
+// Output: [
+//  [".Q..",  // Solution 1
+//   "...Q",
+//   "Q...",
+//   "..Q."],
 
- ["..Q.",  // Solution 2
-  "Q...",
-  "...Q",
-  ".Q.."]
-]
-Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above.
+//  ["..Q.",  // Solution 2
+//   "Q...",
+//   "...Q",
+//   ".Q.."]
+// ]
+// Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above.
 
 var finalResult = solveNQueen(4);
 console.log(finalResult, "Result");
