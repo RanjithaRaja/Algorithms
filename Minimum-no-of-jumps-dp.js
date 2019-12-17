daa0d653e00a13d3c43a964ee4074836c4b28b9e
Ranjitha Raja <ranjitharaja5@gmail.com>
2019-12-17 20:44:18 +0530

    Update Minimum-no-of-jumps-dp.js

--- Minimum-no-of-jumps-dp.js
+++ Minimum-no-of-jumps-dp.js
@@ -1,5 +1,6 @@
   
-// Given an array of integers where each element represents the max number of steps that can be made forward from that element. Write a function to return the minimum number of jumps to reach the end of the array (starting from the first element). 
+// Given an array of integers where each element represents the max number of steps that can be made forward from that element. 
+// Write a function to return the minimum number of jumps to reach the end of the array (starting from the first element). 
 // If an element is 0, then cannot move through that element.
 
 // Example:
@@ -27,5 +28,9 @@ function getMinJump(arr){
     }
   }
   console.log(min_jump_arr)
-  return min_jump_arr[arr.length-1]
+  if(min_jump_arr[arr.length-1] == Infinity){
+    return 0;
+  }else{
+    return min_jump_arr[arr.length-1]
+  } 
 }
